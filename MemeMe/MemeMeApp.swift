cfb9c0fbbcef66808a22e2248a5a4b56c712c10d
KhuePM <minhkhuepham99@gmail.com>
2024-03-25 22:05:09 +0700

    Initial Commit

---
+++ MemeMe/MemeMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMeApp.swift
+//  MemeMe
+//
+//  Created by KhuePM on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemeMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
